0e5699c5f105661ab80c6c9045b39a05c8c61834
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-11-14 22:19:08 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use log::{self, error};
+use serde::{Serialize, Deserialize};
+use simple_error::bail;
+use simple_logger;
+
+#[derive(Deserialize, Clone)]
+struct Event {
+    #[serde(rename = "userId")]
+    user_id: String,
+}
+
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda!(event_handler);
+
+    Ok(())
+}
+
+fn event_handler(e: Event, c: Context) -> Result<CustomOutput, HandlerError> {
+    if e.user_id == "" {
+        error!("Empty user id in request {}", c.aws_request_id);
+        bail!("Empty user id");
+    }
+
+    Ok(CustomOutput {
+        message: format!("Hello, {}!", e.user_id),
+    })
 }
